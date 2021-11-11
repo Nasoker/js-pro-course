afb6b4b74e18a535c6cb22949562a8dcc00e6d77
Nasoker <nasoker@gmail.com>
2021-11-11 11:56:47 +0300

    finish tasks

--- structures/index.js
+++ structures/index.js
@@ -1,17 +1,12 @@
 /* First Task */
- const string = "AaBaaaai";
+  const string = "AaBaaaai";
 const arrayFromString = string.toLowerCase().split('');
 const arrayOfVowels = ['a', 'e', 'i', 'o', 'u'];
 const set = new Set(arrayFromString)
 const arrayWithUniqueLetters = [...set];
-const returnedArray = [];
 
-const result = arrayWithUniqueLetters.filter(letter => {
-    if (arrayOfVowels.includes(letter)) {
-        returnedArray.push(letter)
-    }
-});
-console.log(returnedArray) 
+const result = arrayWithUniqueLetters.filter(letter => arrayOfVowels.includes(letter));
+console.log(result)  
 
 /* Second Task */
 
@@ -31,71 +26,33 @@ setTimeout(() => {
     messages.forEach(message => console.log(getTimeOfMessage(messages)));
 }, 2100);
 
+const newMap = new WeakMap();
+
 function addMessage(message) {
-    const newMap = new Map();
     const date = new Date();
-    newMap.set('text', message);
-    newMap.set('time', date);
+    newMap.set(message, date);
 }
 
 function getTimeOfMessage(message) {
-    return message.get('time');
-}
+    return newMap.get(date);
+} 
 
 
 /* Third Task */
-const defaultHashFunc = (value) => value.toString();
-
-class HashTable {
-    constructor(hashFunction) {
-        this.hashFunction = hashFunction || defaultHashFunc;
-        this.storage = {};
-    }
-
-    add = (value) => {
-        let hash = this.hashFunction(value);
-        if (this.storage[hash] === undefined) {
-            this.storage[hash] = [value];
-        } else {
-            this.storage[hash].push(value);
-        }
-    }
-
-    remove = (value) => {
-        let hash = this.hashFunction(value);
-        if (!this.storage[hash]) {
-            return;
-        } else if (this.storage[hash].length === 1) {
-            delete this.storage[hash];
-        } else {
-            for (let i = 0; i < this.storage[hash].length; i++) {
-                if (this.storage[hash][i][0] === value) {
-                    return delete this.storage[hash][i];
-                }
-            }
-        }
-    }
-
-    lookup = (value) => {
-        let hash = this.hashFunction(value);
-        return this.storage[hash] || [];
-    }
-}
 
 const input = ["tom", "xyz", "mot", "xel", "zXy", "yxz"];
-let arrayOfWords = [];
 
-input.forEach((inputWord) => {
-    arrayOfWords.push(inputWord.toLowerCase().split('').sort().join(''));
-});
+function pushAnagramInArrays(array){
+    const obj = {};
 
-const setOfWords = new Set(arrayOfWords);
-const keys = [...setOfWords]; /* tom, xyz, xel */
-const hashTable = new HashTable();
-const result = arrayOfWords.filter(word => {
-    if (keys.includes(word)) {
-        let index = arrayOfWords.indexOf(word);
-        hashTable.add(input[index]);
-    }
-});
-console.log(hashTable)
+    array.forEach((inputWord) => {
+        const key = inputWord.toLowerCase().split('').sort().join('');
+        if (key in obj){
+            obj[key].push(inputWord);
+        }else{
+            obj[key] = [inputWord]
+        }
+    });
+    return Object.values(obj);
+}
+console.log(pushAnagramInArrays(input))
