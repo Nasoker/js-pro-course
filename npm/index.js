1989825e53aedfe48826385ab505509f9f4512d1
Nasoker <nasoker@gmail.com>
2021-11-18 09:28:06 +0300

    fix errors

--- npm/index.js
+++ npm/index.js
@@ -1,15 +1,7 @@
-/* Second Task */
-var array = [1];
-var other = _.concat(array, 2, [3], [[4]]);
- 
-console.log(other);
- 
-console.log(array); 
 /* Third Task */
 
-const isAuth = () =>{
+const isAuth = () => {
     Math.random() <= 0.5 ? '/.index1.js' : '/.index2.js';
 }
-import(isAuth())
-    .then(console.log(isAuth()))
-
+import (isAuth())
+.then(console.log('Aaa'));
