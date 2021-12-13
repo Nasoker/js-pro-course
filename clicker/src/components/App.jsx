4296c987fd30edfe63960d3a418e8127a7f8b654
Nasoker <nasoker@gmail.com>
2021-12-13 12:44:03 +0300

    finish task

---
+++ clicker/src/components/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import { Clicker } from './clicker/Clicker'
+
+function App() {
+    return (
+        <main>
+            <Clicker/>
+            <Clicker/>
+            <Clicker/>
+            <Clicker/>
+            <Clicker/>
+            <Clicker/>
+        </main>
+    );
+}
+export default App;
